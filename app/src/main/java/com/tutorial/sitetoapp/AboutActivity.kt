0b914623cb210701044f3a27d05ae754783adfc2
Khalid <khalid@windows.com>
2020-06-16 19:27:03 +0100

    add main menu and 3 items

---
+++ app/src/main/java/com/tutorial/sitetoapp/AboutActivity.kt
@@ -0,0 +1,13 @@
+package com.tutorial.sitetoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/**
+ * Created by KHALID EL MRABTI&Khalid Coding on 16/06/20.
+ */
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
